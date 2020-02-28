30b29ff7abec6eb0a26470fcbda6c670534954b3
Marnus Jan Botes <50694994+mjbotes@users.noreply.github.com>
2020-02-28 13:41:14 +0200

    Express and React Configuration

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Start from './Start';
+import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<Start />);
+  const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- stock_track/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Start from './Start';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Start />, document.getElementById('root'));
-
-serviceWorker.unregister();
